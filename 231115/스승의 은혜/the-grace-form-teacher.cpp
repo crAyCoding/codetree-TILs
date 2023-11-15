adbc808436a520a8fc01264bffe5f3b9a3166112
crAy <94739473+crAyCoding@users.noreply.github.com>
2023-11-15 19:15:32 +0900

    Title: 스승의 은혜, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231115/스승의 은혜/the-grace-form-teacher.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N,B;
+    int maxNum = -1, sum = 0, answer = 0;
+    bool flag = true;
+    cin >> N >> B;
+    for(int i=0;i<N;i++)
+    {
+        int p,s;
+        cin >> p >> s;
+        maxNum = maxNum < p ? p : maxNum;
+        sum = sum + p + s;
+        if(sum > B) {
+            if(flag && sum - (maxNum / 2) <= B) {
+                flag = false;
+                sum -= maxNum / 2;
+            }
+            else {
+                answer = i;
+            }
+        }
+    }
+    cout << answer;
+    return 0;
+}
