09e37a130808cedb4b8e70784db2a6b4dee5b833
crAy <94739473+crAyCoding@users.noreply.github.com>
2023-11-15 19:03:45 +0900

    Title: 다양하게 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231115/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Total days in Year" << std::endl
+    std::cout << "365" << std::endl
+    std::cout << "Circumference rate" << std:endl
+    std::cout << "3.1415926535"
+    return 0;
+}
