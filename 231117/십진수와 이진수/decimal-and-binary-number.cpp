289ed8b56f9fe89f8f183653a78800c0c3c5368b
crAy <94739473+crAyCoding@users.noreply.github.com>
2023-11-17 02:40:06 +0900

    Title: 십진수와 이진수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231117/십진수와 이진수/decimal-and-binary-number.cpp
+++ 231117/십진수와 이진수/decimal-and-binary-number.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -12,12 +13,13 @@ int main() {
         c *= 2;
     }
     n *= 17;
-    string result = "";
+    vector<int> v;
     while(n>0) {
-        result += ('0' + n % 2);
+        v.push_back(n%2);
         n /= 2;
     }
-    reverse(result.begin(),result.end());
-    cout << result;
+    for(int i=v.size()-1;i>=0;i--) {
+        cout << v[i];
+    }
     return 0;
 }
