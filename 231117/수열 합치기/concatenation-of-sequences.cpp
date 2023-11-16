764fba89932ef3c539793dfbfbd634b6311708e1
crAy <94739473+crAyCoding@users.noreply.github.com>
2023-11-17 02:17:29 +0900

    Title: 수열 합치기, Time: 122ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 231117/수열 합치기/concatenation-of-sequences.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector<int> v;
+    for(int i=0;i<n+m;i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++) {
+        cout << v[i];
+        if (i == v.size() - 1) break;
+        cout << " ";
+    }
+    return 0;
+}
