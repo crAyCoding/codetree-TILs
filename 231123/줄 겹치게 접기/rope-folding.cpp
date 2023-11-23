ac77f1979d997a730dbd55bafba4f57471c1c739
crAy <94739473+crAyCoding@users.noreply.github.com>
2023-11-23 18:51:14 +0900

    Title: 줄 겹치게 접기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231123/줄 겹치게 접기/rope-folding.cpp
+++ 231123/줄 겹치게 접기/rope-folding.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     int n,l,answer = 0;
     cin >> n >> l;
-    vector<int> v(l+1);
+    vector<int> v(l+1),dots(n);
     for(int i=0;i<n;i++) {
-        int d;
-        cin >> d;
-        v[d] = 1;
+        cin >> dots[i];
+        v[dots[i]] = 1;
     }
-    for(int i=1;i<l;i++) {
-        int cnt = i < l - i ? i : l - i;
-        bool result = true;
-        for(int j=0;j<cnt;j++) {
-            if(v[i-j-1] != v[i+j+1]) {
-                result = false;
+    sort(dots.begin(),dots.end());
+    int left = 0;
+    for(int i=0;i<n;i++) {
+        int temp = i < n-i-1 ? i : n-i-1;
+        bool flag = false;
+        for(int j=0;j<temp;j++) {
+            if(dots[i] - dots[i-j-1] != dots[i+j+1] - dots[i]) break;
+            if(j==temp-1) flag = true;
+        }
+        if(flag) answer++;
+        if(i==n-1) break;
+        float fp = (float)(dots[i+1] + dots[i]) / 2;
+        bool flag2 = true;
+        for(int j=0;j<temp;j++) {
+            float fpl = fp - dots[i-j];
+            float fpr = dots[i+j+1] - fp;
+            if(fpl != fpr) {
+                flag2 = false;
                 break;
             }
         }
-        if(result) answer++;
+        if(flag2) answer++;
     }
     cout << answer;
-    return 0;
 }
