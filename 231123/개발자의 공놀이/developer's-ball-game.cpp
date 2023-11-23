5e0cd002d524780469f7ecaeeaebe19d05f6bd84
crAy <94739473+crAyCoding@users.noreply.github.com>
2023-11-23 23:47:50 +0900

    Title: 개발자의 공놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231123/개발자의 공놀이/developer's-ball-game.cpp
+++ 231123/개발자의 공놀이/developer's-ball-game.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> n;
     vector<int> v(n);
     for(int i=0;i<n;i++) cin >> v[i];
-    sort(v.begin(),v.end());
+    sort(v.begin(),v.end(),greater<int>());
     for(int i=0;i<n-2;i++) {
         while(v[i] - v[i+1] >= v[i+1] - v[i+2]) {
             if(i==n-3) {
